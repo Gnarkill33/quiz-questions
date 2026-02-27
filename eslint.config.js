c061a929b77a837ecac4251d92e15363446f0197
Gnarkill33 <gnarkill33@gmail.com>
2026-02-28 00:23:56 +0300

    chore: add eslint, add ts script

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,75 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import importPlugin from "eslint-plugin-import";
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+export default tseslint.config([
+ { ignores: ["dist"] },
+ {
+  extends: [
+   js.configs.recommended,
+   ...tseslint.configs.recommendedTypeChecked,
+   reactPlugin.configs.flat.recommended,
+   reactPlugin.configs.flat["jsx-runtime"],
+   reactHooks.configs["recommended-latest"],
+   importPlugin.flatConfigs.recommended,
+   eslintConfigPrettier,
+  ],
+  files: ["**/*.{ts,tsx}"],
+  languageOptions: {
+   ecmaVersion: 2020,
+   globals: globals.browser,
+   parserOptions: {
+    projectService: true,
+    tsconfigRootDir: import.meta.dirname,
+   },
   },
-])
+  plugins: {
+   "react-refresh": reactRefresh,
+  },
+  rules: {
+   ...reactHooks.configs.recommended.rules,
+   "@typescript-eslint/consistent-type-exports": "error",
+   "@typescript-eslint/consistent-type-imports": "error",
+   "@typescript-eslint/no-inferrable-types": "error",
+   "@typescript-eslint/array-type": "error",
+   "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+   "react-refresh/only-export-components": [
+    "warn",
+    { allowConstantExport: true },
+   ],
+   "@typescript-eslint/no-explicit-any": "error",
+   "@typescript-eslint/no-magic-numbers": ["error", { ignore: [0, 1, 2] }],
+   "import/order": [
+    "error",
+    {
+     groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+     pathGroups: [
+      {
+       pattern: "@/**",
+       group: "internal",
+      },
+     ],
+     alphabetize: {
+      order: "asc",
+     },
+     "newlines-between": "always",
+    },
+   ],
+  },
+ },
+ {
+  settings: {
+   react: {
+    version: "detect",
+   },
+   "import/resolver": {
+    typescript: true,
+   },
+  },
+ },
+]);
