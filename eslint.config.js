4ee17db7364cfc8163e3456bd937b4a107f09d46
Gnarkill33 <gnarkill33@gmail.com>
2026-03-04 23:47:03 +0300

    fix: fix config files and add extra plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,75 +1,88 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import reactPlugin from "eslint-plugin-react";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-import importPlugin from "eslint-plugin-import";
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import importPlugin from 'eslint-plugin-import';
+import tsParser from '@typescript-eslint/parser';
 
-export default tseslint.config([
- { ignores: ["dist"] },
- {
-  extends: [
-   js.configs.recommended,
-   ...tseslint.configs.recommendedTypeChecked,
-   reactPlugin.configs.flat.recommended,
-   reactPlugin.configs.flat["jsx-runtime"],
-   reactHooks.configs["recommended-latest"],
-   importPlugin.flatConfigs.recommended,
-   eslintConfigPrettier,
-  ],
-  files: ["**/*.{ts,tsx}"],
-  languageOptions: {
-   ecmaVersion: 2020,
-   globals: globals.browser,
-   parserOptions: {
-    projectService: true,
-    tsconfigRootDir: import.meta.dirname,
-   },
-  },
-  plugins: {
-   "react-refresh": reactRefresh,
-  },
-  rules: {
-   ...reactHooks.configs.recommended.rules,
-   "@typescript-eslint/consistent-type-exports": "error",
-   "@typescript-eslint/consistent-type-imports": "error",
-   "@typescript-eslint/no-inferrable-types": "error",
-   "@typescript-eslint/array-type": "error",
-   "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-   "react-refresh/only-export-components": [
-    "warn",
-    { allowConstantExport: true },
-   ],
-   "@typescript-eslint/no-explicit-any": "error",
-   "@typescript-eslint/no-magic-numbers": ["error", { ignore: [0, 1, 2] }],
-   "import/order": [
-    "error",
-    {
-     groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
-     pathGroups: [
-      {
-       pattern: "@/**",
-       group: "internal",
+export default defineConfig([
+  { ignores: ['dist', '**/*.js', '**/*.d.ts', '**/*.config.[jt]s?(x)'] },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
       },
-     ],
-     alphabetize: {
-      order: "asc",
-     },
-     "newlines-between": "always",
     },
-   ],
+    plugins: {
+      react: reactPlugin,
+      'react-refresh': reactRefresh,
+      'react-hooks': reactHooks,
+      import: importPlugin,
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      ...tseslint.configs.recommendedTypeChecked.reduce((acc, config) => ({ ...acc, ...config.rules }), {}),
+      ...reactPlugin.configs.flat.recommended.rules,
+      ...reactPlugin.configs.flat['jsx-runtime'].rules,
+      ...reactHooks.configs['recommended-latest'].rules,
+      ...reactHooks.configs.recommended.rules,
+      ...importPlugin.flatConfigs.recommended.rules,
+      '@typescript-eslint/consistent-type-exports': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-inferrable-types': 'error',
+      '@typescript-eslint/array-type': 'error',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-magic-numbers': ['error', { ignore: [0, 1, 2] }],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          pathGroups: [
+            {
+              pattern: '@/**',
+              group: 'internal',
+            },
+          ],
+          alphabetize: {
+            order: 'asc',
+          },
+          'newlines-between': 'always',
+        },
+      ],
+    },
   },
- },
- {
-  settings: {
-   react: {
-    version: "detect",
-   },
-   "import/resolver": {
-    typescript: true,
-   },
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': { typescript: { project: './tsconfig.json' } },
+    },
   },
- },
+  eslintConfigPrettier,
+  {
+    files: ['**/*.{js,jsx,mjs,cjs}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {},
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
 ]);
