f189e64b9f3ab3069ff3f897c53da458d69bdb1c
Gnarkill33 <gnarkill33@gmail.com>
2026-03-10 23:32:51 +0300

    feat: add question entity layout

--- src/app/layouts/BaseLayout.tsx
+++ src/app/layouts/BaseLayout.tsx
@@ -1,10 +1,13 @@
+import { QuestionItem } from '@/entities/question/ui/QuestionItem/QuestionItem';
 import { Header } from '@/widgets/Header';
 
 export const BaseLayout = () => {
   return (
     <>
       <Header />
-      <div></div>
+      <div>
+        <QuestionItem />
+      </div>
     </>
   );
 };
