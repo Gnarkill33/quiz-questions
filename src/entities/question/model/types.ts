2f3cde153e768a878be7e598f242bb9554d52cd8
Gnarkill33 <gnarkill33@gmail.com>
2026-03-12 21:58:17 +0300

    feat: add types for question, skill, specialization entities

---
+++ src/entities/question/model/types.ts
@@ -0,0 +1,48 @@
+export interface QuestionType {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  code: string | null;
+  imageSrc: string | null;
+  keywords: string[];
+  longAnswer: string | null;
+  shortAnswer: string | TrustedHTML;
+  status: string;
+  rate: number;
+  complexity: number;
+  createdAt: string;
+  updatedAt: string;
+  createdById: string;
+  updatedById: string;
+  createdBy: User;
+  updatedBy: User;
+  questionTopics: string[];
+  questionSpecializations: QuestionSpecialization[];
+  questionSkills: QuestionSkill[];
+}
+
+export interface User {
+  id: string;
+  username: string;
+}
+
+export interface QuestionSpecialization {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  imageSrc: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface QuestionSkill {
+  id: number;
+  title: string;
+  description: string;
+  imageSrc: string;
+  createdAt: string;
+  updatedAt: string;
+  specializations: QuestionSpecialization[];
+}
