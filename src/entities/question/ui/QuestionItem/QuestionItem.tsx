0c3b28918d4063e5fb4fb17c637501a828bc934a
Gnarkill33 <gnarkill33@gmail.com>
2026-03-12 19:33:19 +0300

    feat: add question item layout and styles

--- src/entities/question/ui/QuestionItem/QuestionItem.tsx
+++ src/entities/question/ui/QuestionItem/QuestionItem.tsx
@@ -2,36 +2,45 @@ import styles from './QuestionItem.module.css';
 
 export const QuestionItem = () => {
   return (
-    <li className={styles.accordionItem}>
-      <button aria-expanded="true" className={styles.accordionBtn}>
-        <div className={styles.accordionTitleWrapper}>
-          <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="4" cy="4" r="4" fill="#5533FF" />
-          </svg>
-          <span className={styles.accordionTitle}>Что такое Virtual DOM, и как он работает?</span>
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M6 15L12 9L18 15"
-              stroke="#6A0BFF"
-              strokeWidth="1.66667"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+    <div>
+      <li className={styles.accordionItem}>
+        <button aria-expanded="true" className={styles.accordionBtn}>
+          <div className={styles.accordionTitleWrapper}>
+            <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="4" cy="4" r="4" fill="#5533FF" />
+            </svg>
+            <span className={styles.accordionTitle}>Title</span>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M6 15L12 9L18 15"
+                stroke="#6A0BFF"
+                strokeWidth="1.66667"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+        </button>
+        <div className={styles.accordionBody}>
+          <div className={styles.accordionEvalContainer}>
+            <p className={styles.accordionEvalWrapper}>
+              Рейтинг:
+              <span className={styles.accordionEval}>4</span>
+            </p>
+            <p className={styles.accordionEvalWrapper}>
+              Сложность:
+              <span className={styles.accordionEval}>complexity</span>
+            </p>
+          </div>
+          <img className={styles.questionImg} src="imageSrc" alt="Question image" />
+          {/* <div
+            className={styles.accordionText}
+            dangerouslySetInnerHTML={{
+              __html: question.shortAnswer,
+            }}
+          ></div> */}
         </div>
-      </button>
-      <div className={styles.accordionBody}>
-        <div className={styles.accordionEvalContainer}>
-          <p className={styles.accordionEvalWrapper}>
-            Рейтинг:
-            <span className={styles.accordionEval}>4</span>
-          </p>
-          <p className={styles.accordionEvalWrapper}>
-            Сложность:
-            <span className={styles.accordionEval}>10</span>
-          </p>
-        </div>
-      </div>
-    </li>
+      </li>
+    </div>
   );
 };
