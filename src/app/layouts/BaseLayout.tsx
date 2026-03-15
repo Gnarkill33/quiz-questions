89c18cf88841187976b56ab113a755e3ee2d7459
Gnarkill33 <gnarkill33@gmail.com>
2026-03-15 22:25:58 +0300

    feat: add store provider and render questions to base layout

--- src/app/layouts/BaseLayout.tsx
+++ src/app/layouts/BaseLayout.tsx
@@ -1,12 +1,18 @@
+import { useFetchQuestionsQuery } from '@/entities/question/api/questionApi';
 import { QuestionList } from '@/entities/question/ui/QuestionList/QuestionList';
 import { Header } from '@/widgets/Header';
 
 export const BaseLayout = () => {
+  const { data, error, isLoading } = useFetchQuestionsQuery();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Failed to load questions</div>;
+
   return (
     <>
       <Header />
       <main>
-        <QuestionList questions={[]} />
+        <QuestionList questions={data?.data || []} />
       </main>
     </>
   );
