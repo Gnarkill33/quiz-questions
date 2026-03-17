2b29262d255eacd9a3512f51bd27cb8b0b65a061
Gnarkill33 <gnarkill33@gmail.com>
2026-03-17 17:19:50 +0300

    feat: move fetch questions logic to browser and add page change logic

--- src/app/layouts/BaseLayout.tsx
+++ src/app/layouts/BaseLayout.tsx
@@ -1,18 +1,12 @@
-import { useFetchQuestionsQuery } from '@/entities/question/api/questionApi';
 import { Header } from '@/widgets/Header';
 import { QuestionsBrowser } from '@/widgets/QuestionsBrowser';
 
 export const BaseLayout = () => {
-  const { data, error, isLoading } = useFetchQuestionsQuery();
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Failed to load questions</div>;
-
   return (
     <>
       <Header />
       <main>
-        <QuestionsBrowser questions={data?.data || []} />
+        <QuestionsBrowser />
       </main>
     </>
   );
