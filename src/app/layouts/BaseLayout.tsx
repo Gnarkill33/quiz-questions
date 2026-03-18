498e3faf31932741685117da90bf393bb6780f3d
Gnarkill33 <gnarkill33@gmail.com>
2026-03-18 13:50:32 +0300

    fix: change question browser to outlet to render corresponding pages

--- src/app/layouts/BaseLayout.tsx
+++ src/app/layouts/BaseLayout.tsx
@@ -1,12 +1,13 @@
+import { Outlet } from 'react-router';
+
 import { Header } from '@/widgets/Header';
-import { QuestionsBrowser } from '@/widgets/QuestionsBrowser';
 
 export const BaseLayout = () => {
   return (
     <>
       <Header />
       <main>
-        <QuestionsBrowser />
+        <Outlet />
       </main>
     </>
   );
--- src/app/providers/router.tsx
+++ src/app/providers/router.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from 'react-router';
 
+import { QuestionsPage } from '@/pages/question';
+
 import { BaseLayout } from '../layouts/BaseLayout';
 
 export const router = createBrowserRouter([
   {
     path: '/questions/public-questions',
     element: <BaseLayout />,
+    children: [{ index: true, Component: QuestionsPage }],
   },
 ]);
