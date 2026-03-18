96313da764f17bc5c31dce0e42d0aca011aa56c4
Gnarkill33 <gnarkill33@gmail.com>
2026-03-18 18:10:34 +0300

    feat: add index to shared ui for shorter import paths

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,5 @@
+export { Button } from './Button/Button';
+export { ButtonWrapper } from './ButtonWrapper/ButtonWrapper';
+export { LevelButtons } from './LevelButtons/LevelButtons';
+export { RateButtons } from './RateButtons/RateButtons';
+export { SearchInput } from './SearchInput/SearchInput';
