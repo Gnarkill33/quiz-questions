bb282e6b65e0c2c84f3ff8e8b9fe7632301a9515
Gnarkill33 <gnarkill33@gmail.com>
2026-03-19 23:33:47 +0300

    fix: update button styles and add default icon

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,15 +1,19 @@
 import type { ReactNode } from 'react';
 
+import DefaultImage from '@/shared/assets/images/defaultImg.svg';
+
 import styles from './Button.module.css';
 
 interface Props {
   children: ReactNode;
+  image?: string;
   onClick: () => void;
 }
 
-export const Button = ({ children, onClick }: Props) => {
+export const Button = ({ children, image, onClick }: Props) => {
   return (
     <button className={styles.filterBtn} onClick={onClick}>
+      {image && <img className={styles.img} src={image ? image : DefaultImage} alt="skill-logo" />}
       {children}
     </button>
   );
