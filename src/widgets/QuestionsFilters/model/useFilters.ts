f6301f5bff17430dda1268bb2419d8c0ab4b6ff7
Gnarkill33 <gnarkill33@gmail.com>
2026-03-20 22:01:43 +0300

    feat: add useFilters hook to parse search params and dispatch actions

---
+++ src/widgets/QuestionsFilters/model/useFilters.ts
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router';
+
+import { useAppDispatch, useAppSelector } from '@/app/providers/store';
+
+import { toggleSpecialization } from './filtersSlice';
+
+export const useFilters = () => {
+  const dispatch = useAppDispatch();
+  const filters = useAppSelector((state) => state.filters);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const spec = searchParams.get('slug');
+    if (spec) {
+      dispatch(toggleSpecialization(spec));
+    }
+  }, [dispatch, searchParams]);
+
+  useEffect(() => {
+    setSearchParams((prev) => {
+      const params = new URLSearchParams(prev);
+
+      if (filters.specializationSlug) {
+        params.set('slug', filters.specializationSlug);
+      }
+
+      return params;
+    });
+  }, [filters, setSearchParams]);
+
+  const handleToggleSpecialization = (slug: string) => {
+    dispatch(toggleSpecialization(slug));
+  };
+
+  return {
+    filters,
+    toggleSpecialization: handleToggleSpecialization,
+  };
+};
