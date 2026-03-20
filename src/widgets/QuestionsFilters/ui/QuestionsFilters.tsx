afa4ec7c0b692d74629505e14e1e364fd0070fbb
Gnarkill33 <gnarkill33@gmail.com>
2026-03-20 22:06:21 +0300

    feat: apply useFilters hook to filter questions by spec

--- src/widgets/QuestionsFilters/ui/QuestionsFilters.tsx
+++ src/widgets/QuestionsFilters/ui/QuestionsFilters.tsx
@@ -2,13 +2,17 @@ import { SkillFilter } from '@/features/filter-by-skill/ui/SkillFilter';
 import { SpecializationFilter } from '@/features/filter-by-spec/ui/SpecializationFilter';
 import { LevelButtons, RateButtons, SearchInput } from '@/shared/ui';
 
+import { useFilters } from '../model/useFilters';
+
 import styles from './QuestionsFilters.module.css';
 
 export const QuestionsFilters = () => {
+  const { toggleSpecialization } = useFilters();
+
   return (
     <aside className={styles.filtersContainer}>
       <SearchInput />
-      <SpecializationFilter />
+      <SpecializationFilter onToggle={toggleSpecialization} />
       <SkillFilter />
       <LevelButtons />
       <RateButtons />
