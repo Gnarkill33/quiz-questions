e7373d936dc6e53af38f922bbf44195bfbf8edc9
Gnarkill33 <gnarkill33@gmail.com>
2026-03-23 15:18:21 +0300

    feat: move api to an env constant

--- src/shared/constants/constants.ts
+++ src/shared/constants/constants.ts
@@ -1,3 +1,5 @@
+export const API_URL = import.meta.env.VITE_API_URL as string;
+
 export const LEVELS = ['1-3', '4-6', '7-8', '9-10'];
 export const RATES = ['1', '2', '3', '4', '5'];
 export const SPEC_COUNT_MIN = 5;
