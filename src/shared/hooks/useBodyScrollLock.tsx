fc870aa528ee7c87ac3a26ae5f347983307fea5e
Gnarkill33 <gnarkill33@gmail.com>
2026-03-31 18:35:00 +0300

    feat: add useBodyScroll hook

---
+++ src/shared/hooks/useBodyScrollLock.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+
+export const useBodyScrollLock = (isLocked: boolean) => {
+  useEffect(() => {
+    if (isLocked) {
+      document.body.style.overflow = 'hidden';
+
+      return () => {
+        document.body.style.overflow = '';
+      };
+    }
+  }, [isLocked]);
+};
