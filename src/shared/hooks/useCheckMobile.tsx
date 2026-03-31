a27ec786dc526a29277dec03a74938cf208e717d
Gnarkill33 <gnarkill33@gmail.com>
2026-03-31 18:35:31 +0300

    feat: add useCheckMobile hook

---
+++ src/shared/hooks/useCheckMobile.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+interface Props {
+  breakpoint?: number;
+  onDesktopTransition: () => void;
+}
+
+export const useCheckMobile = ({ breakpoint = 768, onDesktopTransition }: Props) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      const mobile = window.innerWidth <= breakpoint;
+      setIsMobile(mobile);
+      if (!mobile && onDesktopTransition) {
+        onDesktopTransition();
+      }
+    };
+
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+    return () => window.removeEventListener('resize', checkMobile);
+  }, [breakpoint, onDesktopTransition]);
+
+  return isMobile;
+};
