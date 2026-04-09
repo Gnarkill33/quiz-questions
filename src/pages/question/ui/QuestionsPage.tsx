2536658ca50bd221c58d7681a13ca76e4c259472
Gnarkill33 <gnarkill33@gmail.com>
2026-04-09 13:58:35 +0300

    feat: add logic to check non-desktop layout to show filters or overlay

--- src/pages/question/ui/QuestionsPage.tsx
+++ src/pages/question/ui/QuestionsPage.tsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
+
+import { useBodyScrollLock } from '@/shared/hooks/useBodyScrollLock';
+import { useCheckMobile } from '@/shared/hooks/useCheckMobile';
+import { Overlay } from '@/shared/ui/Overlay/Overlay';
 import { QuestionsBrowser } from '@/widgets/QuestionsBrowser';
 import { QuestionsFilters } from '@/widgets/QuestionsFilters';
 
 import styles from './QuestionsPage.module.css';
 
 export const QuestionsPage = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const onDesktopTransition = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const isMobile = useCheckMobile({ breakpoint: 900, onDesktopTransition });
+
+  useBodyScrollLock(isSidebarOpen && isMobile);
+
+  const openSidebar = () => setIsSidebarOpen(true);
+  const closeSideBar = () => setIsSidebarOpen(false);
+
   return (
     <section className={styles.pageContainer}>
-      <QuestionsBrowser />
-      <QuestionsFilters />
+      <QuestionsBrowser isMobile={isMobile} openSidebar={openSidebar} />
+      {isMobile ? (
+        <Overlay closeSideBar={closeSideBar} isSidebarOpen={isSidebarOpen}>
+          <QuestionsFilters />
+        </Overlay>
+      ) : (
+        <QuestionsFilters />
+      )}
     </section>
   );
 };
