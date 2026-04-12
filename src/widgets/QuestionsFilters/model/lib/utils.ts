3037eed4a77d4c13654a5d642e2c1bcc39085e89
Gnarkill33 <gnarkill33@gmail.com>
2026-04-12 21:54:31 +0300

    feat: add utils for useFilters hook

---
+++ src/widgets/QuestionsFilters/model/lib/utils.ts
@@ -0,0 +1,36 @@
+import type { Filters } from '../types';
+
+export const parseFilters = (params: URLSearchParams): Filters => {
+  return {
+    specialization: params.get('specialization') ?? undefined,
+    skills: params.get('skills')?.split(',') ?? [],
+    search: params.get('search') ?? undefined,
+    page: Number(params.get('page') ?? 1),
+  };
+};
+
+export const buildParams = (filters: Filters) => {
+  const params = new URLSearchParams(window.location.search);
+
+  if (filters.specialization) {
+    params.set('specialization', filters.specialization);
+  } else {
+    params.delete('specialization');
+  }
+
+  if (filters.skills?.length) {
+    params.set('skills', filters.skills.join(','));
+  } else {
+    params.delete('skills');
+  }
+
+  if (filters.search) {
+    params.set('search', filters.search);
+  } else {
+    params.delete('search');
+  }
+
+  params.set('page', String(filters.page ?? 1));
+
+  return params;
+};
