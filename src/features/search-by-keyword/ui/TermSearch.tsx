8b8af7b7bfa27726cdba48e3fd750a8188b0e0df
Gnarkill33 <gnarkill33@gmail.com>
2026-04-12 21:51:23 +0300

    feat: add term search feature

--- src/features/search-by-keyword/ui/TermSearch.tsx
+++ src/features/search-by-keyword/ui/TermSearch.tsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from 'react';
 
-// import { useAppSelector } from '@/app/providers/store';
 import { useDebounce } from '@/shared/hooks/useDebounce';
 import { SearchInput } from '@/shared/ui';
 import { useFilters } from '@/widgets/QuestionsFilters/model/hooks/useFilters';
 
 export const TermSearch = () => {
   const { setSearch, filters } = useFilters();
-  // const searchValue = useAppSelector((state) => state.filters.searchValue);
   const [localValue, setLocalValue] = useState(filters.search || '');
   const debouncedValue = useDebounce(localValue, 500);
 
