250b4d5a2d7375a345ca56312bacac3d509b2268
Gnarkill33 <gnarkill33@gmail.com>
2026-04-13 14:45:06 +0300

    fix: remove level buttons component as irrelevant

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,5 +1,4 @@
 export { Button } from './Button/Button';
 export { ButtonWrapper } from './ButtonWrapper/ButtonWrapper';
-export { LevelButtons } from './LevelButtons/LevelButtons';
 export { RateButtons } from './RateButtons/RateButtons';
 export { SearchInput } from './SearchInput/SearchInput';
