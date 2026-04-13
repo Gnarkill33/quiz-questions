c1ce9414c00a6be56a194d84a1c848650f5f1d81
Gnarkill33 <gnarkill33@gmail.com>
2026-04-13 14:44:11 +0300

    fix: change complexity array to object for better use

--- src/shared/constants/constants.ts
+++ src/shared/constants/constants.ts
@@ -1,6 +1,11 @@
 export const API_URL = import.meta.env.VITE_API_URL as string;
 
-export const LEVELS = ['1-3', '4-6', '7-8', '9-10'];
+export const COMPLEXITY = [
+  { label: '1-3', value: ['1', '2', '3'] },
+  { label: '4-6', value: ['4', '5', '6'] },
+  { label: '7-8', value: ['7', '8'] },
+  { label: '9-10', value: ['9', '10'] },
+];
 export const RATES = ['1', '2', '3', '4', '5'];
 export const COUNT_MIN = 5;
 export const SPEC_COUNT_MAX = 28;
